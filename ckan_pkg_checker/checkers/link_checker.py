a48cf92ffad1e88524620259486ac380d22d9933
Maennel <sabine.maennel@liip.ch>
2021-11-01 06:53:37 +0100

    restructuring of the code
    simplfy pkg_checker, so that parameters and config get only checked once
    after that a runparamters named tuple is generated and not changed any more
    
    the steps are better strutured into 1.check, 2. email building, 3. email sending
    more functions are put into the utils and the naming there follows the python
    convention about how to name internal and external functions
    f-strings are used instead of old style string formating
    runs are named, so that they will be better recoginsable

--- ckan_pkg_checker/checkers/link_checker.py
+++ ckan_pkg_checker/checkers/link_checker.py
@@ -1,6 +1,5 @@
 import csv
 import click
-from configparser import ConfigParser
 from collections import namedtuple
 from ckan_pkg_checker.utils import utils
 import ckan_pkg_checker.utils.request_utils as request_utils
@@ -9,16 +8,16 @@ from ckan_pkg_checker.checkers.checker_interface import CheckerInterface
 import logging
 log = logging.getLogger(__name__)
 
-CheckResult = namedtuple('CheckResult', ['pkg', 'resource', 'item', 'msg'])
+CheckResult = namedtuple('CheckResult', ['resource', 'item', 'msg'])
 
 
 class LinkChecker(CheckerInterface):
-    def initialize(self, rundir, config, siteurl):
+    def __init__(self, rundir, config, siteurl):
         """Initialize the link checker"""
         self.url_result_cache = {}
         self.siteurl = siteurl
-        csvfile = utils._get_csvdir(rundir) / utils._get_config(config, 'linkchecker', 'csvfile')
-        msgfile = utils._get_msgdir(rundir) / utils._get_config(config, 'messages', 'msgfile')
+        csvfile = utils.get_csvdir(rundir) / utils.get_config(config, 'linkchecker', 'csvfile')
+        msgfile = utils.get_msgdir(rundir) / utils.get_config(config, 'messages', 'msgfile')
         self._prepare_csv_file(csvfile)
         self._prepare_msg_file(msgfile)
 
@@ -64,14 +63,12 @@ class LinkChecker(CheckerInterface):
                         check_results.append(check_result)
 
         for resource in pkg['resources']:
-            log.info("LINKCHECKER: checking RESOURCE: {}".format(
-                utils._get_field_in_one_language(resource['display_name'], '')
-            ))
+            log.info(f"LINKCHECKER: checking RESOURCE: {utils.get_field_in_one_language(resource['display_name'], '')}")
             resource_results = self._check_resource(pkg, resource)
             if resource_results:
                 check_results.extend(resource_results)
-        for result in check_results:
-            self.write_result(pkg_type, result)
+        for check_result in check_results:
+            self.write_result(pkg, pkg_type, check_result)
 
     def finish(self):
         self.csvfile.close()
@@ -88,17 +85,17 @@ class LinkChecker(CheckerInterface):
             pass
         if resource_url:
             check_result = self._check_url_status(
-                'resource_url', resource_url, pkg, resource)
+                'resource_url', resource_url, resource)
             if check_result:
                 resource_results.append(check_result)
         if download_url and download_url != resource_url:
             check_result = self._check_url_status(
-                'download_url', download_url, pkg, resource)
+                'download_url', download_url, resource)
             if check_result:
                 resource_results.append(check_result)
         return resource_results
 
-    def _check_url_status(self, test_title, test_url, pkg, resource=None):
+    def _check_url_status(self, test_title, test_url, resource=None):
         """Check one url"""
         if test_url in self.url_result_cache:
             test_result = self.url_result_cache[test_url]
@@ -111,26 +108,22 @@ class LinkChecker(CheckerInterface):
             self.url_result_cache[test_url] = test_result
         if test_result:
             check_result = CheckResult(
-                msg=test_result, pkg=pkg,
-                resource=resource, item=test_url)
-            log.info("LINKCHECKER: ERROR: \n{} \nURL {}\nMSG {}".format(
-                test_title, test_url, check_result.msg))
-            click.echo("Linkchecker Error: {} url {} msg {}".format(
-                test_title, test_url, check_result.msg
-            ))
+                msg=test_result, resource=resource, item=test_url)
+            log.info(f"LINKCHECKER: ERROR: \n{test_title} \nURL {test_url}\nMSG {check_result.msg}")
+            click.echo(f"Linkchecker Error: {test_title} url {test_url} msg {check_result.msg}")
             return check_result
 
-    def write_result(self, pkg_type, check_result):
-        contacts = utils._get_pkg_contacts(
+    def write_result(self, pkg, pkg_type, check_result):
+        contacts = utils.get_pkg_contacts(
             check_result.pkg.get('contact_points'))
-        title = utils._get_field_in_one_language(
+        title = utils.get_field_in_one_language(
             check_result.pkg['title'], check_result.pkg['name'])
-        dataset_url = utils._get_ckan_dataset_url(
+        dataset_url = utils.get_ckan_dataset_url(
             self.siteurl, check_result.pkg['name'])
         organization = check_result.pkg.get('organization').get('name')
         resource_url = ''
         if check_result.resource:
-            resource_url = utils._get_ckan_resource_url(
+            resource_url = utils.get_ckan_resource_url(
                 self.siteurl, check_result.pkg['name'],
                 check_result.resource['id'])
         for contact in contacts:
@@ -143,12 +136,12 @@ class LinkChecker(CheckerInterface):
                  'dataset_title': title,
                  'dataset_url': dataset_url,
                  'resource_url': resource_url})
-            msg = utils._build_msg_per_error(
+            msg = utils.build_msg_per_error(
                 check_result.item, check_result.msg,
                 dataset_url, title, resource_url)
             self.mailwriter.writerow({
-                'contact_email': contact.email,
-                'contact_name': contact.name,
+                'contact_email': pkg.get('send_to', contact.email),
+                'contact_name': pkg.get('send_to', contact.name),
                 'pkg_type': pkg_type,
                 'msg': msg,
             })
