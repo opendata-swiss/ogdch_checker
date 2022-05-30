5abac865bdede0756d4494c770a7b9714afdff69
sabinem <sabine.maennel@liip.ch>
2022-05-30 11:06:25 +0200

    fix: spelling error in csv file header

--- statistics/statistics_links.py
+++ statistics/statistics_links.py
@@ -6,7 +6,7 @@ def get_link_statistics():
     sns.set_theme(style="whitegrid", palette="Paired")
     df = pd.read_csv("data/links.csv")
     links_bar_plot = sns.barplot(
-        x="Monat", y="Fehleranzahl", hue="DCAT-Poperty", data=df
+        x="Monat", y="Fehleranzahl", hue="DCAT-Property", data=df
     )
     fig = links_bar_plot.get_figure()
     fig.savefig("plots/links.png")
