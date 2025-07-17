45d90182c6f2d9ade803ff26e7444ccbf3cd4886
kovalch <nataliia.kovalchuk@liip.ch>
2025-07-17 10:59:36 +0200

    feat: Add value from the results_graph

--- ckan_pkg_checker/utils/rdf_utils.py
+++ ckan_pkg_checker/utils/rdf_utils.py
@@ -106,6 +106,9 @@ def get_shacl_results(dataset_graph, shacl_graph, ont_graph):
                 predicate=SHACL.focusNode,
             )
             value = get_object_from_graph(
+                graph=results_graph, subject=validation_item,
+                predicate=SHACL.value
+            ) or get_object_from_graph(
                 graph=dataset_graph, subject=node, predicate=property_ref
             )
             if type(value) == BNode:
